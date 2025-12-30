231ddb38df04534f457906c66b31ae2ab0dfd7ce
Arjun Tiwari <arjuntiwari@Mac.bbrouter>
2025-12-30 12:29:40 +0545

    Initial commit : Add main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int pin; 
+    int correctPin = 1234;
+    int attempts = 0;
+    printf("\nWelcome to ATM\n");
+
+   while (attempts < 3){
+
+     printf("Enter the PIN : ");
+    scanf("%d", &pin);
+
+     if(pin == correctPin){
+        printf("Successfully Entered...");
+       break;
+    }
+    else{
+        attempts++;
+        printf("Wrong PIN. Attempts left : %d \n",3 - attempts );
+    }
+      printf("Invalid PIN. Try again tomorrow");
+   }
+
+   printf("hey hancy boy ");
+   printf("changed");
+   
+    printf("Hey World");
+    return 0;
+}
